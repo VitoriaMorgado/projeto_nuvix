da865932de5b809cecee504703000a2e0acfbabb
Vitoria <vitoriamorgado65@gmail.com>
2025-04-02 20:04:10 -0300

    feat(Home): ajeitando categorias

---
+++ app/comp/carrosselcatalogo/models/cate.ts
@@ -0,0 +1,37 @@
+export interface Cate {
+  id: number;
+  genre: string;
+}
+
+const jogoscategoria: Cate[] = [
+  {
+    id: 1,
+    genre: "SIMULAÇÃO",
+  },
+  {
+    id: 2,
+    genre: "TERROR",
+  },
+  {
+    id: 3,
+    genre: "COPERATIVO",
+  },
+  {
+    id: 4,
+    genre: "CASUAL",
+  },
+  {
+    id: 5,
+    genre: "LUTA",
+  },
+  {
+    id: 6,
+    genre: "FUTURISTA",
+  },
+  {
+    id: 7,
+    genre: "RPG",
+  },
+];
+
+export default jogoscategoria;
